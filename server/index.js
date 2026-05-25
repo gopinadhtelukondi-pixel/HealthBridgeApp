84672d901db12b04215c59c458a127ce7729e78c
Gopi Nadh <gopinadhtelukondigopinadh@gmail.com>
2026-05-25 19:19:05 +0530

    Fixed production CORS policy

--- server/index.js
+++ server/index.js
@@ -23,7 +23,11 @@ connectDB();
 const app = express();
 
 // MIDDLEWARE
-app.use(cors());
+app.use(cors(
+  {origin: "https://healthbridge-app-ruddy.vercel.app/", // Update with your frontend URL
+    methods: "GET,POST,PUT,DELETE",
+  credentials: true}
+));
 app.use(express.json());
 
 // TEST ROUTE
